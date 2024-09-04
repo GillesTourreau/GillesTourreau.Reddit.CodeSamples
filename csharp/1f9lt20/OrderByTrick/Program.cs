b3365249624a828885a0980d6f40fd1cc119b2b2
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2024-09-05 00:00:00 +0200

    [r/csharp] Silly thing that makes me happy | https://www.reddit.com/r/csharp/comments/1f9lt20/silly_thing_that_makes_me_happy/

---
+++ csharp/1f9lt20/OrderByTrick/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderByTrick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var countries = new Country[]
+            {
+                new Country() { Name = "France" },
+                new Country() { Name = "United States of America" },
+                new Country() { Name = "Argentina" },
+                new Country() { Name = "Philippines" },
+                new Country() { Name = "Canada" }
+            };
+
+            var b = new DbContextOptionsBuilder<MyDbContext>();
+            b.UseSqlServer("Data Source=(localdb)\\demoapp; Initial Catalog=Countries; Integrated Security=True");
+
+            var c = new MyDbContext(b.Options);
+
+            var sql = c.Set<Country>().OrderByDescending(c => c.Name == "Canada" || c.Name == "United States of America").ThenBy(c => c.Name).ToArray();
+
+            var order = countries.OrderByDescending(c => c.Name == "Canada" || c.Name == "United States of America").ThenBy(c => c.Name).ToArray();
+
+            Console.WriteLine(order);
+        }
+
+        public class MyDbContext : DbContext
+        {
+            public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Country>();
+            }
+        }
+
+        private sealed class Country
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return this.Name;
+            }
+        }
+    }
+}
--- csharp/README.md
+++ csharp/README.md
@@ -8,3 +8,4 @@ Each entry corresponds to a Reddit post with the related code snippets.
 | Date       | Title                                                                             | Repository folder                                   |
 |------------|-----------------------------------------------------------------------------------|-----------------------------------------------|
 | 2025-09-05 | [Is there an efficient way to copy an entire Http response stream to a file stream while reading the last byte of the stream?](https://www.reddit.com/r/csharp/comments/1f9iexo/is_there_an_efficient_way_to_copy_an_entire_http/) | [1f9iexo](./1f9iexo) |
+| 2025-09-05 | [Silly thing that makes me happy](https://www.reddit.com/r/csharp/comments/1f9lt20/silly_thing_that_makes_me_happy/) | [1f9lt20](./1f9lt20) |
