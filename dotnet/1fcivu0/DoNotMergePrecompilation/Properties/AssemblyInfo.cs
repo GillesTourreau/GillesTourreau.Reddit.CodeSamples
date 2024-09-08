01a2e5b0a594db20be2fd95af7a2e55356f709a1
Gilles TOURREAU <gilles.tourreau@pos-informatique.fr>
2024-09-09 00:00:00 +0200

    [r/dotnet] Pipeline precompile options | https://www.reddit.com/r/dotnet/comments/1fcivu0/pipeline_precompile_options/

---
+++ dotnet/1fcivu0/DoNotMergePrecompilation/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DoNotMergePrecompilation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DoNotMergePrecompilation")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3117a7c0-407d-4e73-8a2a-71cc526c3d69")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- dotnet/README.md
+++ dotnet/README.md
@@ -10,3 +10,4 @@ Each entry corresponds to a Reddit post with the related code snippets.
 | 2024-09-03 | [I don't understand the point of keyed services](https://www.reddit.com/r/dotnet/comments/1f80jd8/i_dont_understand_the_point_of_keyed_services/) | [1f80jd8](./1f80jd8)          |
 | 2024-09-04 | [Checking ownership of related entities](https://www.reddit.com/r/dotnet/comments/1f8na19/checking_ownership_of_related_entities/) | [1f8na19](./1f8na19)          |
 | 2024-09-05 | [Help!!](https://www.reddit.com/r/dotnet/comments/1f9e2l5/help/) | [1f9e2l5](./1f9e2l5)          |
+| 2024-09-09 | [Pipeline precompile options](https://www.reddit.com/r/dotnet/comments/1fcivu0/pipeline_precompile_options/) | [1fcivu0](./1fcivu0)          |
